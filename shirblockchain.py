0156bc515ccc678684f9eef4843c475407f9933f
Hu3in <sh.danishyar@gmail.com>
2020-07-13 15:15:47 +0430

    mining and chain are done everything working

--- shirblockchain.py
+++ shirblockchain.py
@@ -1,9 +1,11 @@
 import json
 import hashlib
-from flask import Flask
+import sys
 from time import time
+from uuid import uuid4
+from flask import Flask, jsonify, request
 
-class BlockChian():
+class BlockChain():
     """ define a Block chain on one machine
     i have to define in other machine later 
 
@@ -12,14 +14,14 @@ class BlockChian():
         self.chain = []
         self.current_transactions = []
         # here for new block i use (1) andn for the first one proof is not matter so i use randomly for example 100
-        self.new_block(previous_hash=1, proof=100)
+        self.new_block(prvious_hash=1, proof=100)
 
     def new_block(self, proof, prvious_hash=None):
         """ create a new block for our chain 
 
         constructor for the 'block' is :
         index: a unique ID for every block 
-        timestamp: Time of generating a block 
+        timestamp: Time of generating a btransactionlock 
         transactions: List of Transactions is here 
         previous hash: in every new generating block we should put previous hash of that block 
         this is the one which make secure our block it means that if someone hack one block soe it means that he/she need to hack 
@@ -43,7 +45,7 @@ class BlockChian():
         # so we created a block we should make sure that our mempool is empty so i will do it like this 
         self.current_transactions = []
         # i need to append the the new created block to our chian 
-        self.chian.append(block)
+        self.chain.append(block)
         return block
 
 
@@ -64,13 +66,13 @@ class BlockChian():
         # or sorting is not important, but in blockchain the ordring is imporant so i use ( sort_key=True)
         # encode is show that it will make a human readable 
         """
-        block_string = json.dumps(block, sort_key=True).encode()
+        block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
         """ return the last block of block chain        """
-        pass
+        return self.chain[-1]
     
     @staticmethod
     def valid_proof(last_proof, proof):
@@ -90,4 +92,64 @@ class BlockChian():
         proof = 0
         while self.valid_proof(last_proof, proof) is False:
             proof += 1
-        return proof 
+        return proof
+        
+
+# here i use Flask instead of Django becausenew_i need just a restfull API so Flask is enough 
+app = Flask(__name__)
+
+# becuase i wanna make it on netwook so here i create a node_id 
+node_id = str(uuid4())
+
+blockchain = BlockChain()
+
+@app.route("/mine", methods=["GET"])
+def mine():
+    """ this will mine one block and will added to the chain """
+    # because we wanna start mining form last block so here we go 
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # after we find the last_proof so i need to add a coin to my self as a rewrad which you can see in bitcoin
+    # in bitcoin before every 4 yeaer the amont of reward for every block will be haf 
+    # it means that before 2002 it was 12.5, but after 2020 it is 6.25, intially for bitoin it was 50 coin for every block
+    # here the sender is my self, and the recipient is also my self wich is my node_id
+
+    blockchain.new_transaction(sender="0", receiver=node_id, amount=50)
+
+    # right now i have added coin to my self so i need to create a block
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+    response = {
+        "message ": " new Block created ",
+        "index": block["index"],
+        "transactions": block["transactions"],
+        "proof": block["proof"],
+        "previous_hash": block["previous_hash"]
+    }
+    return jsonify(response), 200
+
+
+@app.route("/transaction/new", methods=["POST"])
+def new_transaction():
+    """ i will add a new transaction 
+    by getting sender , receiver and amount 
+    """
+    values = request.get_json()
+    this_block = blockchain.new_transaction(values['sender'],values['receiver'], values['amount'])
+    response = {"message": f"will be added to block {this_block}"}
+    return jsonify(response), 201 # 201 means done
+@app.route("/chain")
+def full_chain():
+    """ return the full chain"""
+    response = {
+        "chain": blockchain.chain,
+        "length": len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=sys.argv[1])
+    
